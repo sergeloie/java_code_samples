c5efa348f9268d8eccb07da8e173d6da30ab5fbc
sergeloie <serge.loie@yandex.ru>
2023-09-18 22:20:09 +0500

    Update List_and_Stream.java

--- List_and_Stream.java
+++ List_and_Stream.java
@@ -1,5 +1,6 @@
 //получаем из List поток целых чисел и помещаем нечётные в начало, а чётные в конец нового List
 // https://ru.hexlet.io/challenges/s_basic_algorithms_in_place_sorting_exercise/  
+// list.stream().forEach можно заменить на list.forEach
 public static List<Integer> run(List<Integer> list) {
 
         List<Integer> result = new ArrayList<>();
