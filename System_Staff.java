6f3b2b7d835daab6bed34894b5562eec5d97d5e9
Sergey Gusev <serge.loie@yandex.ru>
2023-11-01 14:42:11 +0500

    Update System_Staff.java

--- System_Staff.java
+++ System_Staff.java
@@ -4,3 +4,10 @@ Runtime runtime = Runtime.getRuntime();
 // Получение размера heap space
 long totalHeapSpaceSize = runtime.totalMemory() / (1024 * 1024); // переводим в мегабайты
 System.out.println("Размер heap space: " + totalHeapSpaceSize + " MB");
+
+
+// отобразить время работы приложения
+long startTime = System.currentTimeMillis();
+
+long endTime = System.currentTimeMillis();
+System.out.printf("Execution time: %s %n", new DurationFormatUtils().formatDuration(endTime - startTime, "HH:MM:ss.SS"));
