a9275b4e1ae65fa63db0921ecccac82c794baebe
Sergey Gusev <serge.loie@yandex.ru>
2023-11-01 11:28:11 +0500

    Create Format_Data.java

---
+++ Format_Data.java
@@ -0,0 +1,38 @@
+// как вывести количество байтов в человекочитаемом формате
+
+import static org.apache.commons.io.FileUtils.byteCountToDisplaySize;
+
+//или
+
+// From: https://programming.guide/worlds-most-copied-so-snippet.html
+public static strictfp String humanReadableByteCount(long bytes, boolean si) {
+    int unit = si ? 1000 : 1024;
+    long absBytes = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+    if (absBytes < unit) return bytes + " B";
+    int exp = (int) (Math.log(absBytes) / Math.log(unit));
+    long th = (long) Math.ceil(Math.pow(unit, exp) * (unit - 0.05));
+    if (exp < 6 && absBytes >= th - ((th & 0xFFF) == 0xD00 ? 51 : 0)) exp++;
+    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+    if (exp > 4) {
+        bytes /= unit;
+        exp -= 1;
+    }
+    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+}
+
+// или проще Binary (1 Ki = 1,024)
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %ciB", value / 1024.0, ci.current());
+    }
