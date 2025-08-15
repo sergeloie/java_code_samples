268ac16c05348b3ea084ac6580c9e2e2a2e1a61a
Sergey Gusev <serge.loie@yandex.ru>
2025-08-15 14:26:56 +0300

    Update String.java

--- String.java
+++ String.java
@@ -6,3 +6,10 @@ public String reverseUsingStringBuilder(String input) {
 
     return new StringBuilder(input).reverse().toString();
 }
+
+//разделить предложение на слова, без пробелов и прилепившихся знаков препинания
+string.split("[!?',;.\\s]+")
+
+// и  собрать в мапу, в нижнем регистре, с подсчётом количества  слов
+        Map<String, Long> map = Arrays.stream(paragraph.split("[!?',;.\\s]+"))
+                .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()));    
